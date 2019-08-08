deb8d1cea00793c3cd53396034aeac42abef1a95
Mukhtar Mahamed <bilemansoor@gmail.com>
2019-08-08 09:23:45 +0300

    Updates

---
+++ precache-manifest.cae65d40d0af07f4edf3214a8bc9e049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ebb87beaa108138e1029b6c762f9442",
+    "url": "/sostech/index.html"
+  },
+  {
+    "revision": "096ab6cbc97eebf54cab",
+    "url": "/sostech/static/css/2.cf8b194c.chunk.css"
+  },
+  {
+    "revision": "e041335547a9d2904ed8",
+    "url": "/sostech/static/css/main.ad5b5347.chunk.css"
+  },
+  {
+    "revision": "096ab6cbc97eebf54cab",
+    "url": "/sostech/static/js/2.3ae4f2e3.chunk.js"
+  },
+  {
+    "revision": "e041335547a9d2904ed8",
+    "url": "/sostech/static/js/main.cb49e6bd.chunk.js"
+  },
+  {
+    "revision": "42cac19def8ba6cde493",
+    "url": "/sostech/static/js/runtime~main.81ed32f6.js"
+  }
+]);
